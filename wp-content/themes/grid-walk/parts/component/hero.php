626912b304a329a2b96d6f235d80a96f648ca8c5
Jamie Moore <jamiemoore2689@gmail.com>
2021-08-08 21:13:01 +0100

    mark up for homepage hero

---
+++ wp-content/themes/grid-walk/parts/component/hero.php
@@ -0,0 +1,13 @@
+<section class="homepage-hero">
+    <div class="homepage-hero__img">
+        <img src="<?php echo get_theme_file_uri('/img/hero-image.jpg'); ?>" alt="#">
+    </div>
+    <div class="homepage-hero__content">
+        <h2 class="homepage-hero__title">Engineered Beauty</h2>
+        <p class="homepage-hero__content">We love motosport and we love design. Here is a collection of the most beautiful liveries that have ever graced the grid.</p>
+        <a href="#" class="btn">View liveries</a>
+    </div>
+
+
+</section>
+
