c0b02a2d820a513cb6388df08adef1a572ec5671
Jamie Moore <jamiemoore2689@gmail.com>
2021-08-16 16:34:37 +0100

    built homepage hero module

--- wp-content/themes/grid-walk/parts/component/hero.php
+++ wp-content/themes/grid-walk/parts/component/hero.php
@@ -1,13 +1,21 @@
-<section class="homepage-hero">
-    <div class="homepage-hero__img">
-        <img src="<?php echo get_theme_file_uri('/img/temp/hero-image.jpg'); ?>" alt="#">
-    </div>
+<section class="container homepage-hero section-spacer">
+    <picture class="homepage-hero__img">
+        <?php
+            $image = get_field('hero_image');
+
+            $mob = $image['sizes'][ 'home-hero-image--mob' ];
+            $desktop = $image['sizes'][ 'home-hero-image' ];
+        ?>
+        <source srcset="<?php echo esc_url($mob); ?>" media="(max-width: 640px)" />
+        <img src="<?php echo esc_url($desktop); ?>" alt="<?php echo $image['alt'];?>">
+    </picture>
     <div class="homepage-hero__content">
-        <h2 class="homepage-hero__title">Engineered Beauty</h2>
-        <p class="homepage-hero__content">We love motosport and we love design. Here is a collection of the most beautiful liveries that have ever graced the grid.</p>
-        <a href="#" class="btn">View liveries</a>
+        <h2 class="homepage-hero__title h1"><?php the_field('hero_title');?></h2>
+        <p><?php the_field('hero_text'); ?></p>
+        <?php if(get_field('enable_button')) { ?>
+            <div class="homepage-hero__btn-contain">
+                <a href="<?php the_field('hero_button_location');?>" class="btn btn--fill"><?php the_field('hero_button_text');?></a>
+            </div>
+        <?php } ?>
     </div>
-
-
 </section>
-
