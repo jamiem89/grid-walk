290b460f0e8ee644d4c91e831280abbd69575d90
Jamie Moore <jamiemoore2689@gmail.com>
2021-08-16 16:35:04 +0100

    Created site footer

--- wp-content/themes/grid-walk/parts/global/footer.php
+++ wp-content/themes/grid-walk/parts/global/footer.php
@@ -1,4 +1,27 @@
+<footer class="container site-footer">
+    <div class="site-footer__grid">
+        <h5 class="site-logo"><a href="/">Grid Walk</a></h5>
+        <?php wp_nav_menu(array(
+            'theme_location' => 'footerMenu',
+            'menu_class' => 'site-footer__menu',
+            'container' => ''
+        )); ?>
+        <!-- <li><a href="#">List item one</a></li>
+        <li><a href="#">List item two</a></li>
+        <li><a href="#">List item three</a></li> -->
+        <?php wp_nav_menu(array(
+            'theme_location' => 'footerMenuLegal',
+            'menu_class' => 'site-footer__menu',
+            'container' => ''
+        )); ?>
+        <div class="site-footer__legal">
+            <p class="text-small">&copy; <?php echo date('Y'); ?> Grid Walk.</p>
+        </div>
+    </div>
 
-<?php wp_footer();?>
+</footer>
+<?php wp_footer(); ?>
+</div>
 </body>
+
 </html>
